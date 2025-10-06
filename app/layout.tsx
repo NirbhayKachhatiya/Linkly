4dd5f46c58af765c307eca2ba511a94ad347c33f
NirbhayKachhatiya <nirbhaykck@gmail.com>
2025-10-06 16:59:47 +0530

    added tinybird for analytics and docker

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <ClerkProvider dynamic>
           <ConvexClientProvider>{children}</ConvexClientProvider>
         </ClerkProvider>
